38e1995e3e6847218bcea118192e0c51f01bc089
Apa73ch <70505469+Apa73ch@users.noreply.github.com>
2025-03-27 12:23:28 -0400

    Improve login ui

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -16,7 +16,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   Future<void> _login() async {
     setState(() => isLoading = true);
-    
+
     String email = emailController.text.trim();
     String password = passwordController.text.trim();
 
@@ -32,22 +32,21 @@ class _LoginScreenState extends State<LoginScreen> {
       final response = await http.post(
         Uri.parse("http://raspberrypi2.local/login.php"),
         headers: {"Content-Type": "application/json"},
-        body: jsonEncode({
-          "email": email,
-          "password": password
-        }),
+        body: jsonEncode({"email": email, "password": password}),
       );
 
       setState(() => isLoading = false);
 
       if (response.statusCode == 200) {
         var jsonResponse = jsonDecode(response.body);
-        
+
         if (jsonResponse["success"]) {
           SharedPreferences prefs = await SharedPreferences.getInstance();
-          await prefs.setString("token", jsonResponse["token"] ?? ""); // Guarda token o vacío
+          await prefs.setString(
+              "token", jsonResponse["token"] ?? ""); // Guarda token o vacío
 
-          Navigator.pushReplacementNamed(context, "/main"); // Redirige al dashboard
+          Navigator.pushReplacementNamed(
+              context, "/main"); // Redirige al dashboard
         } else {
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(content: Text("Error: ${jsonResponse['message']}")),
@@ -71,77 +70,91 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       backgroundColor: const Color(0xFFEFF3FF),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(Icons.bug_report, size: 100, color: Colors.black),
-              const SizedBox(height: 10),
-              Text(
-                "SmartTrap",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.blue.shade900),
-              ),
-              const SizedBox(height: 20),
-
-              _buildTextField("Usuario/Correo Electrónico", emailController, false),
-              const SizedBox(height: 15),
-              _buildTextField("Contraseña", passwordController, true),
-              const SizedBox(height: 15),
-
-              ElevatedButton(
-                onPressed: isLoading ? null : _login,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue.shade900,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                  padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 15),
-                ),
-                child: isLoading
-                    ? const CircularProgressIndicator(color: Colors.white)
-                    : const Text("Continuar", style: TextStyle(fontSize: 18, color: Colors.white)),
-              ),
-              const SizedBox(height: 20),
-
-              Row(
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 400),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Text("¿No tienes cuenta?"),
-                  const SizedBox(width: 5),
+                  const Icon(Icons.bug_report, size: 100, color: Colors.black),
+                  const SizedBox(height: 10),
+                  Text(
+                    "SmartTrap",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue.shade900),
+                  ),
+                  const SizedBox(height: 20),
+                  _buildTextField(
+                      "Usuario/Correo Electrónico", emailController, false),
+                  const SizedBox(height: 15),
+                  _buildTextField("Contraseña", passwordController, true),
+                  const SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: isLoading ? null : _login,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue.shade900,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 100, vertical: 15),
+                    ),
+                    child: isLoading
+                        ? const CircularProgressIndicator(color: Colors.white)
+                        : const Text("Continuar",
+                            style:
+                                TextStyle(fontSize: 18, color: Colors.white)),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("¿No tienes cuenta?"),
+                      const SizedBox(width: 5),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => RegisterScreen()),
+                          );
+                        },
+                        child: const Text(
+                          "Regístrate aquí",
+                          style: TextStyle(
+                              color: Colors.blue, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
                   GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => RegisterScreen()),
-                      );
-                    },
+                    onTap: () {}, // Implementar recuperación de contraseña
                     child: const Text(
-                      "Regístrate aquí",
-                      style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+                      "Olvidé mi contraseña",
+                      style: TextStyle(
+                          color: Colors.blue, fontWeight: FontWeight.bold),
                     ),
                   ),
                 ],
               ),
-              const SizedBox(height: 10),
-
-              GestureDetector(
-                onTap: () {}, // Implementar recuperación de contraseña
-                child: const Text(
-                  "Olvidé mi contraseña",
-                  style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  Widget _buildTextField(String label, TextEditingController controller, bool isPassword) {
+  Widget _buildTextField(
+      String label, TextEditingController controller, bool isPassword) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(label, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+        Text(label,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
         const SizedBox(height: 5),
         TextField(
           controller: controller,
@@ -150,7 +163,9 @@ class _LoginScreenState extends State<LoginScreen> {
             filled: true,
             fillColor: Colors.white,
             hintText: isPassword ? "************" : "User@gmail.com",
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: BorderSide.none),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide.none),
           ),
         ),
       ],
